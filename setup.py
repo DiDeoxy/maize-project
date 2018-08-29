a4441eaafee5ccf1629d96669c8d0222ebf3b614
max hargreaves <max_hargreaves@outlook.com>
2018-08-29 11:51:57 -0400

    trying to get my windows environment setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Modules',
+      version='0.2',
+      description='Modules for maize project',
+      author='Max Hargreaves',
+      author_email='whargrea@uoguelph.ca',
+      package_dir={'': 'modules'},
+      )
